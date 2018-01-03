2a9b06897a78addebfc89b1a2f604562ab924ab1
Ryan Workman <rdavid1099@gmail.com>
2018-01-03 11:54:00 -0700

    Add sad path tests for language assignment

--- test/new_test.sh
+++ test/new_test.sh
@@ -15,9 +15,30 @@ setup()
   [ "${lines[0]}" = "Building JavaScript sprint named simple_sprint" ]
 }
 
-@test "it assigns language value when proper flag is passed" {
+@test "it assigns language value when --language flag is passed" {
   run bash $NEW --language=ruby
 
   [ "$status" -eq "0" ]
-  [ "${lines[0]}" = "Building Ruby sprint named simple_sprint" ]  
+  [ "${lines[0]}" = "Building Ruby sprint named simple_sprint" ]
+}
+
+@test "it assigns language value when -l flag is passed" {
+  run bash $NEW -l python
+
+  [ "$status" -eq "0" ]
+  [ "${lines[0]}" = "Building Python sprint named simple_sprint" ]
+}
+
+@test "it throws error with unsupported languages" {
+  run bash $NEW -l golang
+
+  [ "$status" -eq "1" ]
+  [ "${lines[0]}" = "ERROR: Unsupported language 'golang' passed. Please pass a supported language." ]
+}
+
+@test "it throws error when multiple languages are passed" {
+  run bash $NEW -l ruby --language=javascript
+
+  [ "$status" -eq "1" ]
+  [ "${lines[0]}" = "ERROR: Multiple parameters passed for '--language'. Only one argument can be passed for each flag." ]
 }
